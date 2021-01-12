c6758a964944c68fe01fc1a04ad1c5de4fe4502a
Randall Oshita <54516717+randalloshita@users.noreply.github.com>
2021-01-12 09:27:43 -0800

    Update AddADUser.ps1

--- ActiveDirectory/AddADUser.ps1
+++ ActiveDirectory/AddADUser.ps1
@@ -6,10 +6,6 @@ Write-host "Welcome. Create AD users here. This module only creates new accounts
 
 $Credential = Get-Credential
 
-#Initials
-Write-host "Enter your initials for checkmarking:"
-$Initials = Read-Host
-
 #Get files info
 $EmployeeChecklistFolder = "P:\EMPLOYEE CHECKLIST\Z_Pending"
 
@@ -66,24 +62,21 @@ Do
 #Convert Password to secure string
 $NewADUserPassword = 'Abcd1234'
 $SecureString = ConvertTo-SecureString $NewADUserPassword -AsPlainText -Force
-
-#Create ADUser
-Invoke-Command -ComputerName PCBDC1 -Credential $Credential -ScriptBlock {New-ADUser -SamAccountName $SamAccountName -Instance $ADUserInstance.name -Title $Title -Department $BranchDepartment -DisplayName $DisplayName -Name $Name -AccountPassword $SecureString -Description $Title}    
-
-#Wait 5 minutes to sync new user object throughout the domain.
-start-sleep -second 300
+New-ADUser -SamAccountName $SamAccountName -Instance $ADUserInstance.name -Title $Title -Department $BranchDepartment -DisplayName $DisplayName -Name $Name -AccountPassword $SecureString -Description $Title    
 
 #Modify Home Directory
 $ADUser = Invoke-Command -ComputerName PCBDC1 -Credential $Credential -Scriptblock {Get-ADUser -filter "name -like '*$USing:SamAccountName*'" -properties *}
 $ADUserHomeDirectory = $ADUser.HomeDirectory
 
-#Create Home Directory and apply permissions
-New-Item -Path $ADUserHomeDirectory -Type directory -Credential $Credential
+New-Item -Path $ADUserHomeDirectory -Type directory
 New-SmbShare -Name $SamAccountName -Path $ADUserHomeDirectory -FullAccess "Everyone"
-#perissions
-Set-ACL -Path $ADUserHomeDirectory 
 
-#Create mailbox
+#Wait 5 minutes for the DCs to sync up before creating new objects that reference New AD account.
+start-stop -second 300
+
+#Add permissions to Home Directory
+
+#Add mailbox
 Write-host "Do you want to create a mailbox? [Y] Yes, [N] No."
 $CreateMailbox = Read-Host
 
@@ -91,7 +84,6 @@ If ($CreateMailbox -eq 'Y')
 {
     #New mailbox
     Invoke-Command -ComputerName PCBEXCHANGE -Credential $Credential -ScriptBlock {New-Mailbox -Name $FullName -UserPrincipalName $UserPrincipalName -Alias $UserPrincipalName} 
-    write-host "Mailbox created."
 }
 Else
 {
@@ -100,16 +92,17 @@ Else
 
 
 #Fill out Employee Checklist form
+
 #$Excel = Open-ExcelPackage -Path "P:\EMPLOYEE CHECKLIST\Z_Pending\New Employee Checklist_2020-9-1_TEST, TESTOR_IT.xlsx"
 $Excel = Open-ExcelPackage -Path $EmployeeChecklistPath
 $Worksheet = $Excel.Workbook.Worksheets["Employee Checklist (For IT)"]
 $Worksheet.Cells['C11'].Value = $SamAccountName 
 $Worksheet.Cells['C14'].Value = $SameAccountName + '@paccitybank.com'
 $Worksheet.Cells['C15'].Value = $ADUserHomeDirectory
-$Worksheet.Cells['J11'].Value = $Initials
-$Worksheet.Cells['J12'].Value = $Initials
-$Worksheet.Cells['J14'].Value = $Initials
-$Worksheet.Cells['J15'].Value = $Initials
+$Worksheet.Cells['J11'].Value = 'RO'
+$Worksheet.Cells['J12'].Value = 'RO'
+$Worksheet.Cells['J14'].Value = 'RO'
+$Worksheet.Cells['J15'].Value = 'RO'
 
 
 
